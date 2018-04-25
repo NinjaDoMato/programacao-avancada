326bca8d926f7d8b09357fc0cf31a3a34cf481f4
Daniel Deda <37677288+NinjaDoMato@users.noreply.github.com>
2018-04-25 18:39:40 -0300

    Update main.cpp

--- src/trabalho3/main.cpp
+++ src/trabalho3/main.cpp
@@ -10,6 +10,7 @@ int main() {
 
     int i;
 
+    //Empilha utilizando operador +=
     pilha1 += "D";
     pilha1 += "a";
     pilha1 += "n";
@@ -20,6 +21,7 @@ int main() {
     pilha1.Imprime();
     int tam = pilha1.Tamanho();
 
+    //Desempilha utiliando operador --
     for(i = 0; i < tam; i++)
     {
         pilha1--;
@@ -28,6 +30,7 @@ int main() {
 
     cout<<"Trabalho 3 - Ponto"<<endl;
 
+    //Criação dos objetos Ponto
     Ponto p1(1, 2);
     Ponto p2(3, 4);
     Ponto p3(4, 6);
@@ -36,8 +39,10 @@ int main() {
     cout<<"P2: "<<p2[0]<<", "<<p2[1]<<endl;
     cout<<"P3: "<<p3[0]<<", "<<p3[1]<<endl;
 
+    //Aplicação dos operadores
     Ponto res = (p1*3)+p2-p3/(p1+p2);
 
+    //Resultado
     res.imprime();
 
     return 0;
