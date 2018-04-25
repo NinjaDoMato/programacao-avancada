336eda6a8c1c4f455bff542e49dbe4e8a2d95437
Daniel Deda <37677288+NinjaDoMato@users.noreply.github.com>
2018-04-25 18:34:41 -0300

    Update Conta.h

--- src/trabalho2/Conta.h
+++ src/trabalho2/Conta.h
@@ -11,21 +11,25 @@ class Conta
 {
 public:
 
-    Conta() : numero(++numero), saldo(0.0), cliente("")
+    //Construtor vazio
+    Conta() : numero(0), saldo(0.0), cliente("")
     {
         cout<<"Criando conta "<<numero<<" do cliente "<<cliente.getNome()<<endl;
     };
+    
+    //Construtor com atributos
     Conta(int numero, Cliente cliente) : numero(numero), saldo(0.0), cliente(cliente)
     {
         cout<<"Criando conta "<<numero<<" do cliente "<<cliente.getNome()<<endl;
     }
 
+    //Métodos
     virtual inline void Deposita(float valor) = 0;
     virtual inline void Retira(float valor) = 0;
     virtual inline void Transfere(Conta &contaDestino, float valor) = 0;
     virtual inline void const Extrato() = 0;
     virtual inline void AplicaJurosDiarios(int dias) = 0;
-
+   
 protected:
 
     int numero;
